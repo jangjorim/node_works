957a751256fde25bd3440cd4e29ed9d7230887fa
jangjorim <smin8006@gmail.com>
2020-09-15 12:04:24 +0900

    helloworld 세줄

--- Chap1/HelloWorld2.js
+++ Chap1/HelloWorld2.js
@@ -4,6 +4,7 @@ var server = http.createServer(function(request, response){
     response.writeHead(200,{'Content-Type' : 'text/html'});
     response.end('<h1>Hello world!!</h1>');
     response.end('<h1>Hello world!!</h1>');
+    response.end('<h1>Hello world!!</h1>');
 });
 
 server.listen(3000);//port 번호 : 프로세스를 구분시켜주기 위해서
